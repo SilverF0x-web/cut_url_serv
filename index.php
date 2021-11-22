33d24e802f750f1025deb790044545165b452378
SilverF0x-web <lugovnin@inbox.ru>
2021-11-22 12:29:22 +0300

    login/logout session, password hash, register, profile

--- index.php
+++ index.php
@@ -1,44 +1,41 @@
-<?php include "includes/header.php"?>
-
 <?php
     if (isset($_GET['url']) && !empty($_GET['url'])) {
         $url = strtolower(trim($_GET['url']));
 
-        $link = db_query("SELECT * FROM `links` WHERE `short_link` = '$url';")->fetch();
+        $link = get_link_info($url);
 
         if (empty($link)) {
-            header('Location: ' . "404.php");
+            header('Location: 404.php');
             die;
         }
 
-        db_exec("UPDATE `links` SET `views` = `views` + 1 WHERE `short_link` = '$url';");
+        update_views($url);
         header('Location: ' . $link['long_link']);
         die;
-
-    } else {
-
     }
+
+    include "includes/header.php";
 ?>
-	<main class="container">
-		<div class="row mt-5">
-			<div class="col">
-				<h2 class="text-center">Необходимо <a href="<?php echo get_url('register.php'); ?>">зарегистрироваться</a> или <a href="<?php echo get_url('login.php'); ?>">войти</a> под своей учетной записью</h2>
-			</div>
-		</div>
-		<div class="row mt-5">
-			<div class="col">
-				<h2 class="text-center">Пользователей в системе: <?php echo $users_count ?></h2>
-			</div>
-		</div>
-		<div class="row mt-5">
-			<div class="col">
-				<h2 class="text-center">Ссылок в системе: <?php echo $links_count ?></h2>
-			</div>
-		</div>
-		<div class="row mt-5">
-			<div class="col">
-				<h2 class="text-center">Всего переходов по ссылкам: <?php echo $views_count ?></h2>
-			</div>
-		</div>
-	</main>
+<main class="container">
+    <div class="row mt-5">
+        <div class="col">
+            <h2 class="text-center">Необходимо <a href="<?php echo get_url('register.php'); ?>">зарегистрироваться</a> или <a href="<?php echo get_url('login.php'); ?>">войти</a> под своей учетной записью</h2>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col">
+            <h2 class="text-center">Пользователей в системе: <?php echo $users_count ?></h2>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col">
+            <h2 class="text-center">Ссылок в системе: <?php echo $links_count ?></h2>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col">
+            <h2 class="text-center">Всего переходов по ссылкам: <?php echo $views_count ?></h2>
+        </div>
+    </div>
+</main>
 <?php include "includes/footer.php"?>
